e29c5c7e51aaf0d676326f8311715082903b3176
dancun48 <111135378+dancun48@users.noreply.github.com>
2025-07-16 23:45:37 +0300

    git commit -m "[commit message]"

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 function myFunction() {
     var x = document.getElementById("myNavbar");
     if (x.className === "navbar") {
-        x.className += " responsive";
+        x.className += "responsive";
     } else {
         x.className = "navbar";
     }
 }
+/*
+const slides = document.getElementById("slides");
+    const totalSlides = slides.children.length;
+    let index = 0;
+
+    function showSlide(i) {
+        index = (i + totalSlides) % totalSlides;
+        slides.style.transform = `translateX(-${index * 100}%)`;
+    }
+
+    function nextSlide() {
+        showSlide(index + 1);
+    }
+
+    function prevSlide() {
+        showSlide(index - 1);
+    }
+
+    // Auto slide every 4 seconds
+   // setInterval(() => nextSlide(), 1000);
+
+*/
